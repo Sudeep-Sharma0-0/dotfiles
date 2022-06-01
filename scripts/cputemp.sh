2ddfbb298f5342618c19712a6d26dfad125e251b
Sudeep-Sharma0-0 <sharma.sudip1122@gmail.com>
2022-06-01 23:42:54 +0545

    Updated dot files. New dwm version.

--- scripts/cputemp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sensors | grep "Package id 0:" | awk '{printf("%.0f°C",substr($4,2))}'
--- scripts/memhtop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-free_output=$(free -m | grep Mem)
-
-MEMUSED=$(echo $free_output | awk '{print $3}')
-MEMTOT=$(echo $free_output | awk '{print $2}')
-
-printf " %sMB" "$MEMUSED"
